ab24d9ddbf810ca481c650c850c57254d5a17b33
Domagoj Bakota <domagojbakota@gmail.com>
2019-11-09 15:55:44 +0100

    created usermodel

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """test creating a new user with an email is successful"""
+        email = 'domagojbakota@gmail.com'
+        password = 'kolikoje'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        ) 
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_meail_normalize(self):
+        """test the email for a new user is normalized"""
+        email = "test@DEMAGOGUE.com"
+        user = get_user_model().objects.create_user(email, 'kolikoje')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test creating user wit no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """ test creating a new superuser """
+        user = get_user_model().objects.create_superuser(
+            'test@demagogue.com',
+            'kolikoje'
+            )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
